64dbf63bc017407f6639e4c88aba32b81eb5d1ee
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-09-18 12:47:42 -0700

    put placeholders for profile page

--- src/Components/profile/profile.jsx
+++ src/Components/profile/profile.jsx
@@ -6,7 +6,14 @@ const Profile = () => {
     //change password
   return (
     <div>
-      
+      <div className="login-redirect-home">
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+      </div>
+      <h1>
+        You are a cool cat
+      </h1>
     </div>
   )
 }
