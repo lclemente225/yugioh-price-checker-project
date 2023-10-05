775c7114ac399bc7497dd8934e225b325746dc8d
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-10-05 12:47:31 -0700

    debugging shoplist

--- src/Components/shoplist/shoplist.jsx
+++ src/Components/shoplist/shoplist.jsx
@@ -31,6 +31,7 @@ const { isLoading, error, data, refetch } = useQuery('Yugioh Cart Data',
 async () =>{
       let response =  await fetch(`/.netlify/functions/functions/cart/list`);
       let data = await response.json();   
+      console.log("trying to load cart")
           return data
           },{refetchOnWindowFocus: false},[]);
           
