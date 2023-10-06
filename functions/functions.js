f4c976cc69d012f973c3db91f2e591ddee3ead42
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-10-05 22:59:22 -0700

    edited functions

--- functions/functions.js
+++ functions/functions.js
@@ -3,33 +3,18 @@ import serverless from 'serverless-http';
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
 
-const allowedOrigins = [
-        'https://main--ygo-pricechecker.netlify.app', 
-        'http://localhost:5173', 
-        'https://ygo-pricechecker.netlify.app'
-  ];
+
+const router = express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const corsOptions = {
-    origin: function (origin, callback) {
-                if (allowedOrigins.includes(origin) || !origin) {
-                    callback(null, true);
-                } else {
-                    callback(new Error('Not allowed by CORS'));
-                }
-            },
-};
 
-// Use the CORS middleware with the specified options
-app.use(cors(corsOptions));
 
 // https://ygo-pricechecker.netlify.app/.netlify/functions/functions/dude
-app.get('/dude', (req, res) => {
+router.get('/dude', (req, res) => {
     console.log("works")
     return res.send("This is workjing")
 })
