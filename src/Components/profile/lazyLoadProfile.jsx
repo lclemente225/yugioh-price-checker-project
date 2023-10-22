ae61c9d68ed95dc7376e02b4f72aebc094743e04
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-10-22 15:38:32 -0700

    edited lazyLoad

--- src/Components/profile/lazyLoadProfile.jsx
+++ src/Components/profile/lazyLoadProfile.jsx
@@ -1,13 +1,13 @@
 import { lazy } from "react";
 
 export default function LazyLoad (path, namedExport) {
-    return lazy(() => {
+    return lazy(async () => {
         const promise = import(path);
         if(namedExport == null){
             return promise
         } else {
             return promise.then(module => {
-                return ({ default: module[namedExport] })
+                return { default: module[namedExport] }
             })
         }
     })
