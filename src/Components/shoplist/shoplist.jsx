1666c9523464493e98ee1de7c60e35afa88ad00b
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-10-29 21:15:27 -0700

    added querycache

--- src/Components/shoplist/shoplist.jsx
+++ src/Components/shoplist/shoplist.jsx
@@ -36,7 +36,7 @@ const queryCache = new QueryCache({
     console.log("settled cache", data)
   }
 })
-queryCache.finaAll(['Yugioh Cart Data'])
+queryCache.findAll(['Yugioh Cart Data'])
 const { isLoading, error, data, refetch } = useQuery('Yugioh Cart Data', 
       async () =>{
                 let response =  await fetch(`/.netlify/functions/functions/cart/list`);
