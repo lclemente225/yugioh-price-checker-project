21e28873adb8966c69e4646450ca3f0f95575ead
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-10-31 02:03:07 -0700

    edited profile user and email edit fn

--- src/Components/profile/profile.jsx
+++ src/Components/profile/profile.jsx
@@ -56,7 +56,7 @@ const Profile = () => {
         }, 
         body: JSON.stringify(profileFormData)
       }).then((res) => {
-        console.log("Successfully Edited Username")
+        console.log("Successfully Edited Username", res.message)
       }).catch((error) => {
         console.log("Error Editing USer:", error) 
       })
@@ -73,7 +73,7 @@ const Profile = () => {
         },
         body: JSON.stringify(profileFormData)
       }).then((res) => {
-        console.log("Successfully Edited Email", res.json());
+        console.log("Successfully Edited Email", res.message);
       }).catch((error) => {
         console.log("Error Editing Email:", error) 
       })
