27891201a6cdb2cc059ab4eacefddc83bdc47ba9
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-10-30 23:06:58 -0700

    deleted cachetime

--- src/Components/shoplist/shoplist.jsx
+++ src/Components/shoplist/shoplist.jsx
@@ -44,8 +44,7 @@ const { isLoading, error, data, refetch } = useQuery('Yugioh Cart Data',
                 return data
           },{
             refetchOnWindowFocus: false,
-            notifyOnChangeProps: ['Yugioh Cart Data'],
-            cacheTime: 1
+            notifyOnChangeProps: ['Yugioh Cart Data']
           },
           []);
           
