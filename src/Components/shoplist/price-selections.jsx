7d031325309da7100555d6579d0a75e6dcbf229a
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-11-13 08:48:22 -0800

    added price selections js

---
+++ src/Components/shoplist/price-selections.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const PriceSelections = () => {
+  return (
+    <div>
+        What prices do you want to check out?
+        <p>
+        TCG Player
+        </p>
+        <p>
+        Amazon
+        </p>
+        <p>
+        Ebay
+        </p>
+        <p>
+        
+        </p>
+        <p>
+        CardMarket
+        </p>
+  </div>
+  )
+}
+
+export default PriceSelections
