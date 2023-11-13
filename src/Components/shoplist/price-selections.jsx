6a9258c6230c985155372ff1cf21b5d980d3a34e
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-11-13 10:37:53 -0800

    added price selections

--- src/Components/shoplist/price-selections.jsx
+++ src/Components/shoplist/price-selections.jsx
@@ -1,23 +1,75 @@
 import React from 'react'
 
-const PriceSelections = () => {
+
+const PriceSelections = ({selectedPrice, choosePriceSelection}) => {
+
+  function selectAmazon(){
+    choosePriceSelection({
+      amazon_price: true, 
+      cardmarket_price: false, 
+      coolstuffinc_price: false, 
+      ebay_price: false,
+      tcgplayer_price: false
+    })
+  }
+  function selectCardMarket(){
+    choosePriceSelection({
+      amazon_price: false, 
+      cardmarket_price: true, 
+      coolstuffinc_price: false, 
+      ebay_price: false,
+      tcgplayer_price: false
+    })
+  }
+  function selectCoolStuff(){
+    choosePriceSelection({
+      amazon_price: false, 
+      cardmarket_price: false, 
+      coolstuffinc_price: true, 
+      ebay_price: false,
+      tcgplayer_price: false
+    })
+  }
+  function selectEbay(){
+    choosePriceSelection({
+      amazon_price: false, 
+      cardmarket_price: false, 
+      coolstuffinc_price: false, 
+      ebay_price: true,
+      tcgplayer_price: false
+    })
+  }
+  function selectTCG(){
+    choosePriceSelection({
+      amazon_price: false, 
+      cardmarket_price: false, 
+      coolstuffinc_price: false, 
+      ebay_price: false,
+      tcgplayer_price: true
+    })
+  }
   return (
-    <div>
+    <div className='price-list-container'>
         What prices do you want to check out?
-        <p>
-        TCG Player
+        <p  className={`${ selectedPrice.tcgplayer_price ? 'selected-price' : ''} price-selection`}
+        onClick={() => selectTCG()}>
+            TCG Player
         </p>
-        <p>
-        Amazon
+        <p  className={`${selectedPrice.amazon_price ? 'selected-price' : ''} price-selection`}
+        onClick={() => selectAmazon()}>
+            Amazon
         </p>
-        <p>
-        Ebay
+        <p  className={`${selectedPrice.ebay_price ? 'selected-price' : ''} price-selection`}
+        onClick={() => selectEbay()}>
+            Ebay
         </p>
-        <p>
-        
+        <p  className={`${selectedPrice.coolstuffinc_price ? 'selected-price' : ''} price-selection`}
+        onClick={() => selectCoolStuff()}>
+            CoolStuffInc
         </p>
-        <p>
-        CardMarket
+        <p  className={`${selectedPrice.cardmarket_price ? 'selected-price' : ''} price-selection`}
+        onClick={() => selectCardMarket()}>
+            CardMarket
         </p>
   </div>
   )
