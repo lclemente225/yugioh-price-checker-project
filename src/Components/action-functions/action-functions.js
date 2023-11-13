adcb7fa837470ac972ca47fedbf0480022e03ad4
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-11-13 00:48:26 -0800

    edited addToCartInCart fn

--- src/Components/action-functions/action-functions.js
+++ src/Components/action-functions/action-functions.js
@@ -5,7 +5,6 @@ export async function addToCartinCart(e, name, price, cartId, userId){
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({  "card_name": name, 
-                              "cardmarket_price":price["cardmarket_price"], 
                               "quantity":"1",
                               "cartId": cartId,
                               "userId":userId
