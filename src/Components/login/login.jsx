7c894b6f5bd2b8b03ddbe0101894169707418a27
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-11-21 12:34:21 -0800

    checking for cookie obj in loginjsx

--- src/Components/login/login.jsx
+++ src/Components/login/login.jsx
@@ -37,7 +37,7 @@ function Login({LogIn, isLoggedIn,givenUserId, setUserId}) {
         console.log("Login successful! Showing Login Data", 
          loginData.Login,
          loginData.email,
-         cookies
+         JSON.parse(cookies)
          );
          
       } else {
