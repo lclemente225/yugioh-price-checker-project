9e671f01b076949a6a1ad61492f9786c3c2e37af
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2024-01-10 00:34:12 -0800

    testing console log in add cart fn

--- src/Components/mainpage/mainpage.jsx
+++ src/Components/mainpage/mainpage.jsx
@@ -1,5 +1,5 @@
 import NavBar from '../navbar/navbar';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { toast } from 'react-hot-toast';
 import "./mainpage.css";
