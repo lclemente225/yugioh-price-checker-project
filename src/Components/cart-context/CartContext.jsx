6fde7991990e40c5f6acddd5505e48be6db0e65f
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2024-02-26 12:17:45 -0800

    added fetch error handling

--- src/Components/cart-context/CartContext.jsx
+++ src/Components/cart-context/CartContext.jsx
@@ -8,7 +8,9 @@ export const CartProvider = ({children}) => {
     const {isLoading, error, data} = useQuery('Yugioh Cart Data', 
           async () =>{
                     let response =  await fetch(`/.netlify/functions/functions/cart/list`);
-                      
+                    if (!response.ok) {
+                        throw new Error('Failed to fetch data');
+                    }
                     console.log("trying to load cart context", response)
                     return response.json()
               }, []);
