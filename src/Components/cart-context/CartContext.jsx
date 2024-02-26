a259f14b70a4a4f371a9956221254927790979a7
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2024-02-26 10:37:14 -0800

    added error checks in cart context

--- src/Components/cart-context/CartContext.jsx
+++ src/Components/cart-context/CartContext.jsx
@@ -5,13 +5,22 @@ export const CartContext = createContext(null);
 
 export const CartProvider = ({children}) => {
 
-    const {data} = useQuery('Yugioh Cart Data', 
+    const {isLoading, error, data} = useQuery('Yugioh Cart Data', 
           async () =>{
                     let response =  await fetch(`/.netlify/functions/functions/cart/list`);
                     let data = await response.json();   
                     console.log("trying to load cart context", data)
                     return data
               }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    // Check if there's an error
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
 
     return (
         <CartContext.Provider value={data}>
