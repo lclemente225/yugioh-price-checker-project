8f87113d7d077580a6baf2ee6af062c59864b28e
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2024-02-26 12:15:57 -0800

    changed back to usequery

--- src/Components/cart-context/CartContext.jsx
+++ src/Components/cart-context/CartContext.jsx
@@ -1,35 +1,20 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext } from "react";
 import { useQuery } from 'react-query';
 
 export const CartContext = createContext(null);
 
 export const CartProvider = ({children}) => {
 
-    const [data, setData] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const {isLoading, error, data} = useQuery('Yugioh Cart Data', 
+          async () =>{
+                    let response =  await fetch(`/.netlify/functions/functions/cart/list`);
+                      
+                    console.log("trying to load cart context", response)
+                    return response.json()
+              }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('/.netlify/functions/functions/cart/list');
-                if (!response.ok) {
-                    throw new Error('Failed to fetch data');
-                }
-                const jsonData = await response.json();
-                setData(jsonData);
-            } catch (error) {
-                setError(error);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-
-        fetchData();
-    }, []);
-    
     if (isLoading) {
-        return <div>Loading...</div>
+        return <div>Loading...</div>;
     }
 
     // Check if there's an error
